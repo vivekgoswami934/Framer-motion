b99013864df6b8f664e4f639190eca75bb737ff1
vivekgoswami934 <vivek@rhombuz.io>
2024-09-04 05:53:17 +0530

    added the talwind css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProgressBar from "./components/animateAndTransition/ProgressBar";
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <ProgressBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/animateAndTransition/ProgressBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <div>
+       <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
+    </div>
+  );
+};
+
+export default ProgressBar;
