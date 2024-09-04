bde7db9660161677364b96c9ae0de17d6d0844f2
vivekgoswami934 <vivek@rhombuz.io>
2024-09-04 06:13:51 +0530

    created the progress bar by motion comp.

--- src/components/animateAndTransition/ProgressBar.jsx
+++ src/components/animateAndTransition/ProgressBar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const ProgressBar = () => {
   return (
-    <div>
-       <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
-    </div>
+    <>
+      <h1 className="text-zinc-300 text-3xl">Progress Bar</h1>
+      <div className="mt-6">
+        <div className="w-full h-2 rounded bg-white">
+          <motion.div
+            animate={{
+              width: "100%",
+            }}
+            transition={{ ease: "easeOut", duration: 4 }}
+            className="w-1/12 h-full bg-teal-700"
+          ></motion.div>
+        </div>
+      </div>
+    </>
   );
 };
 
